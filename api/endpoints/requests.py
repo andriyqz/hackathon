60068a698e166d93e4528931a85ed609b164de58
andriyqz <andrijlisun92@gmail.com>
2026-04-03 16:35:13 +0300

    feat: added requests filtering

--- api/endpoints/requests.py
+++ api/endpoints/requests.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi import Body
 from sqlalchemy.orm import Session
 
-from typing import List
+from typing import List, Optional
 
+from repositories.filters import BaseFilter, EqualsFilter, GreaterThanFilter, LessThanFilter
 from services.request_service import RequestService
-from schemas.request import RequestCreate, RequestResponse
+from schemas.request import RequestCreate, RequestResponse, RequestStatus
+from models.enums import Priority
 from core.dependencies import get_db
 
 router = APIRouter()
@@ -15,10 +17,32 @@ def get_request_service(db: Session = Depends(get_db)):
 
 
 @router.get('/', response_model=List[RequestResponse])
-def get_requests(request_service: RequestService = Depends(get_request_service)):
-    return request_service.get_new_requests()
+def get_requests(
+    request_service: RequestService = Depends(get_request_service),
+    status: Optional[RequestStatus] = Query(None),
+    min_id: Optional[int] = Query(None),
+    max_id: Optional[int] = Query(None),
+    priority: Optional[Priority] = Query(None)
+):
+    mapping = {
+        "status": {"class": EqualsFilter, "field": "status"},
+        "min_id": {"class": GreaterThanFilter, "field": "id"},
+        "max_id": {"class": LessThanFilter, "field": "id"},
+        "priority": {"class": EqualsFilter, "field": "priority"}
+    }
+    
+    params = {
+        "status": status,
+        "min_id": min_id,
+        "max_id": max_id,
+        "priority": priority
+    }
+    
+    filters = apply_filters(params, mapping)
+    return request_service.get_requests(filters=filters)
 
 
 @router.post('/', response_model=RequestResponse)
-def create_request(user_id: int, request_data: RequestCreate = Body(...), request_service: RequestService = Depends(get_request_service)):
+def create_request(user_id: int, request_data:  RequestCreate = Body(...), 
+                   request_service: RequestService = Depends(get_request_service)):
     return request_service.create_request(user_id=user_id, data=request_data)
